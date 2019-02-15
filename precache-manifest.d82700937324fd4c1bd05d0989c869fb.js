747a8c3ec2a630e04fbd0e36b57b3e68bc60f413
Yurii <yurii.oleshko@gmail.com>
2019-02-15 18:09:03 +0200

    Updates

---
+++ precache-manifest.d82700937324fd4c1bd05d0989c869fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea66546006255e80124d",
+    "url": "/Blog_zdec/static/js/runtime~main.ea665460.js"
+  },
+  {
+    "revision": "aa5c6cac3133fec5146c",
+    "url": "/Blog_zdec/static/js/main.aa5c6cac.chunk.js"
+  },
+  {
+    "revision": "4a26403cece3b0c9e24c",
+    "url": "/Blog_zdec/static/js/1.4a26403c.chunk.js"
+  },
+  {
+    "revision": "aa5c6cac3133fec5146c",
+    "url": "/Blog_zdec/static/css/main.c5fc02eb.chunk.css"
+  },
+  {
+    "revision": "bae676bac74b38bdad615798bd62fbdb",
+    "url": "/Blog_zdec/index.html"
+  }
+];
